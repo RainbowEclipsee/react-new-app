64141848e32358bd644a1291c011462d3366941c
RainbowEclipsee <dexterrullez1@gmail.com>
2022-10-27 21:29:23 +0300

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Counter from './components/Counter'
 
 function App() {
+  const [like, setLike] = useState(0)
+  const [value, setValue] = useState('Введите текст')
+
+  function increment() {
+    setLike(like + 1)
+  }
+  function decrement() {
+    setLike(like - 1)
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{like}</h1>
+      <h1>{value}</h1>
+      <input
+        type="text"
+        value={value}
+        onChange={(event) => setValue(event.target.value)}
+      ></input>
+      <button onClick={increment}>Увеличить</button>
+      <button onClick={decrement}>Уменьшить</button>
+      {/* Reusable Сomponents */}
+      <Counter />
+      <Counter />
+      <Counter />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
