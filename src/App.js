c6145aa0865288e8c3c4bd199fe00ea7c4f68f33
RainbowEclipsee <dexterrullez1@gmail.com>
2022-11-01 21:06:32 +0300

    PostList

--- src/App.js
+++ src/App.js
@@ -1,35 +1,42 @@
 import React, { useState } from 'react'
-import ClassCounter from './components/ClassCounter'
-import Counter from './components/Counter'
+// import ClassCounter from './components/ClassCounter'
+// import Counter from './components/Counter'
+// import PostItem from './components/PostItem'
+import PostList from './components/PostList'
+import MyButton from './components/UI/button/MyButton'
+import MyInput from './components/UI/input/MyInput'
+import './styles/App.css'
 
 function App() {
-  const [like, setLike] = useState(0)
-  const [value, setValue] = useState('Введите текст')
+  const [posts, setPosts] = useState([
+    { id: 1, title: 'JavaScript 1', body: 'Пост рассказывает о JavaScript' },
+    { id: 2, title: 'JavaScript 2', body: 'Пост рассказывает о JavaScript' },
+    { id: 3, title: 'JavaScript 3', body: 'Пост рассказывает о JavaScript' },
+    { id: 4, title: 'JavaScript 4', body: 'Пост рассказывает о JavaScript' },
+    { id: 5, title: 'JavaScript 5', body: 'Пост рассказывает о JavaScript' },
+    { id: 6, title: 'JavaScript 6', body: 'Пост рассказывает о JavaScript' },
+  ])
 
-  function increment() {
-    setLike(like + 1)
-  }
-  function decrement() {
-    setLike(like - 1)
+  const [title, setTitle] = useState('Бла-бла-бла')
+
+  const addNewPost = (a) => {
+    a.preventDefault()
   }
 
   return (
     <div className="App">
-      <h1>{like}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      ></input>
-      <button onClick={increment}>Увеличить</button>
-      <button onClick={decrement}>Уменьшить</button>
-      {/* Reusable Сomponents */}
-      <Counter />
-      <Counter />
-      <Counter />
-      <ClassCounter />
-      <ClassCounter />
+      <form>
+        {/* Управляемый компонент на onChange реализовано двуст.связывание*/}
+        <MyInput
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          placeholder="Название поста"
+        ></MyInput>
+        <MyInput type="text" placeholder="Описание поста"></MyInput>
+        <MyButton onClick={addNewPost}>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title={'Список постов про Js'} />
     </div>
   )
 }
