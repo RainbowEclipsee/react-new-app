37992c1992cc587a0d0321e41be99af5a9d8b263
RainbowEclipsee <dexterrullez1@gmail.com>
2022-11-08 22:37:21 +0300

    added - selection sort

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostForm from './components/PostForm'
 // import Counter from './components/Counter'
 // import PostItem from './components/PostItem'
 import PostList from './components/PostList'
+import MySelect from './components/UI/select/MySelect'
 // import MyButton from './components/UI/button/MyButton'
 // import MyInput from './components/UI/input/MyInput'
 import './styles/App.css'
@@ -26,6 +27,7 @@ function App() {
       body: 'Пост рассказывает о JavaScript',
     },
   ])
+  const [selectedSort, setSelectedSort] = useState('')
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
   }
@@ -33,10 +35,28 @@ function App() {
   const removePost = (post) => {
     setPosts(posts.filter((p) => p.id !== post.id))
   }
+  const sortPosts = (sort) => {
+    setSelectedSort(sort)
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
+  }
 
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <div>
+        <hr style={{ margin: '15px 0' }} />
+      </div>
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Сортировка"
+          options={[
+            { value: 'title', name: 'По названию' },
+            { value: 'body', name: 'По описанию' },
+          ]}
+        />
+      </div>
       {posts.length !== 0 ? (
         <PostList
           remove={removePost}
